e5eee16c16769356a2d3ee655f1d0b80e62c379b
nimsimethusala <nimsimethusala42@gmail.com>
2025-06-11 19:48:35 +0530

    add role base access implementations

--- Payment_Service/src/main/java/lk/ijse/payment_service/controller/PaymentController.java
+++ Payment_Service/src/main/java/lk/ijse/payment_service/controller/PaymentController.java
@@ -4,6 +4,7 @@ import lk.ijse.payment_service.dto.PaymentRequestDTO;
 import lk.ijse.payment_service.entity.Payment;
 import lk.ijse.payment_service.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -15,11 +16,13 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping("/pay")
+    @PreAuthorize("hasAnyRole('USER','OWNER')")
     public Payment makePayment(@RequestBody PaymentRequestDTO request) {
         return paymentService.processPayment(request);
     }
 
     @GetMapping("/booking/{bookingId}")
+    @PreAuthorize("isAuthenticated()")
     public Payment getPaymentByBooking(@PathVariable String bookingId) {
         return paymentService.getByBookingId(UUID.fromString(bookingId));
     }
